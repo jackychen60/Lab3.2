c86e5b5c44032e9d626bcd64c5868aab09cf0ffa
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2018-02-05 09:13:00 -0500

    afgfgs

---
+++ MergePartition/src/Sorter.java
@@ -0,0 +1,17 @@
+
+public class Sorter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	public static int[] merge(int[]list1,int[]list2)
+	{
+		
+	}
+	public static int partition(int[]list)
+	{
+		
+	}
+
+}
