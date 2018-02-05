cb3fd34be6e1fde53b26ed88b500355b25aecaf2
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2018-02-05 09:23:23 -0500

    lkl

--- MergePartition/src/Sorter.java
+++ MergePartition/src/Sorter.java
@@ -1,12 +1,14 @@
-
+//Jacky Chen 
+//Period 2
 public class Sorter {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 	}
-	public static int[] merge(int[]list1,int[]list2)
+	public static String[] merge(String[]list1,String[]list2)
 	{
+		
 		
 	}
 	public static int partition(int[]list)
