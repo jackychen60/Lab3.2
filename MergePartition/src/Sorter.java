b2270bf8fe9d5745092faca3d96a971184801a00
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2018-02-06 09:23:38 -0500

    asd

--- MergePartition/src/Sorter.java
+++ MergePartition/src/Sorter.java
@@ -1,19 +1,47 @@
+import java.util.Arrays;
+
 //Jacky Chen 
 //Period 2
 public class Sorter {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		String[] test1 = { "apple", "banana", "dog", "egg", "grape", "poop" };
+		String[] test2 = { "snake", "umbrella", "van", "watermelon", "yoyo", "zebra"};
+		long start = System.nanoTime();
+		merge(test1, test2);
+		long end = System.nanoTime();
+		long time = end - start;
+		System.out.println("Merge Took :" + time + "nanoseconds");
+		System.out.println(Arrays.toString(test1));
 	}
 	public static String[] merge(String[]list1,String[]list2)
 	{
-		
+		String empty[] = new String[list1.length + list2.length];
+		int a=0;
+		for(int i=0;i<list1.length;i++)
+		{
+			for(int j=0;j<list2.length;j++)
+				{
+				if(list1[i].compareTo(list2[j])<0)
+				{
+					empty[a]=list1[i];
+					
+				}
+				else if(list1[i].compareTo(list2[j])>0)
+				{
+					empty[a]=list2[j];
+					
+				}
+					
+				}
+		}
+		return empty;
 		
 	}
-	public static int partition(int[]list)
+	/*public static int partition(int[]list)
 	{
 		
 	}
-
+*/
 }
