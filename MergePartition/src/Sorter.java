fea0a950ea2def89e48651cd701fe6c7d1f1a90e
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2018-02-07 09:23:39 -0500

    12345

--- MergePartition/src/Sorter.java
+++ MergePartition/src/Sorter.java
@@ -7,34 +7,35 @@ public class Sorter {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String[] test1 = { "apple", "banana", "dog", "egg", "grape", "poop" };
-		String[] test2 = { "snake", "umbrella", "van", "watermelon", "yoyo", "zebra"};
+		String[] test2 = { "cat", "umbrella", "van", "watermelon", "yoyo", "zebra"};
 		long start = System.nanoTime();
 		merge(test1, test2);
 		long end = System.nanoTime();
 		long time = end - start;
 		System.out.println("Merge Took :" + time + "nanoseconds");
-		System.out.println(Arrays.toString(test1));
+		System.out.println(Arrays.toString(merge(test1, test2)));
 	}
 	public static String[] merge(String[]list1,String[]list2)
 	{
 		String empty[] = new String[list1.length + list2.length];
-		int a=0;
-		for(int i=0;i<list1.length;i++)
+		
+		for(int i=0;i<=list1.length;i++)
 		{
+			int a=0;
+			
 			for(int j=0;j<list2.length;j++)
 				{
-				if(list1[i].compareTo(list2[j])<0)
+				if(list1[i].compareTo(list2[j])>0)
 				{
 					empty[a]=list1[i];
-					
-				}
-				else if(list1[i].compareTo(list2[j])>0)
+					a++;
+									}
+				if(list1[i].compareTo(list2[j])<0)
 				{
 					empty[a]=list2[j];
-					
-				}
-					
-				}
+					a++;
+									}
+					}
 		}
 		return empty;
 		
